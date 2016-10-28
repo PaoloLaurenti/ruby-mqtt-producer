842a04ce257f65703cc88360879dbccf771a01dc
Paolo Laurenti <laurentipaolo@gmail.com>
2016-10-28 09:32:04 +0200

    Add docker management

---
+++ docker/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+l
+function buildDockerImage()
+{
+  cp -R "../src/" .
+
+  if [ ! -z "$HTTP_PROXY" ]; then
+      docker build --build-arg HTTP_PROXY=$HTTP_PROXY -t ggp/ruby_mqtt_producer .
+    else
+      docker build -t ggp/ruby_mqtt_producer .
+  fi
+}
